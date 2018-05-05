06afd9c9a755b98c9195b66f592268b58e077515
drew <drewdrewthis@gmail.com>
2018-05-05 12:09:06 -0400

    Move rails and react app to own repos

--- rails-server/app/graphql/types/todo_type.rb
+++
@@ -1,8 +0,0 @@
-Types::TodoType = GraphQL::ObjectType.define do
-  name 'Todo'
-
-  # it has the following fields
-  field :id, !types.ID
-  field :title, !types.String
-  field :description, !types.String
-end
